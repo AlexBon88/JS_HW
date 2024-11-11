67d8affe9ce65e4abb2a48f027bd07c1878da343
AlexBon88 <bonakhov2107@gmail.com>
2024-11-11 20:31:15 +0200

    homework 4 is ready

--- dz4/dz4_10/scripts/main.js
+++ dz4/dz4_10/scripts/main.js
@@ -1,5 +1,5 @@
 function minimum(digits) {
-    let min = digits([0]);
+    let min = digits[0];
     for (let i = 1; i < digits.length; i++) {
         const digit = digits[i];
         if (digit<min) {
